a68014be62e7af7ad6dbd641cee8a8a76f002f52
AlexViti <alexviti12@gmail.com>
2022-07-12 16:34:10 +0200

    Add 3 products

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using (ShopContext db = new ShopContext())
+{
+    for (int i = 1; i < 4; i++)
+    {
+        Product product = new Product
+        {
+            Name = "Product " + i,
+            Price = 10.00M * i,
+            Description = "This is a product description"
+        };
+        db.Products.Add(product);
+    }
+    db.SaveChanges();
+}
